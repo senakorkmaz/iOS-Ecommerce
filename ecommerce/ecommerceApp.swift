3a097f9db9c70b99a33980ee0c1d60cebc98bbe4
Senanur Korkmaz <62392202+senakorkmaz@users.noreply.github.com>
2023-06-22 20:56:14 +0300

    Upload

---
+++ ecommerce/ecommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerceApp.swift
+//  ecommerce
+//
+//  Created by Senanur Korkmaz on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ecommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
